089a085e8812316cc00c8befa61f8c252a272f43
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-22 14:33:13 +0000

    [TASK] Enhance extension's description
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cloudflare/trunk@62695 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'CloudFlare Client',
-	'description' => 'Implementation of the CloudFlare Client Interface API.',
+	'description' => 'Implementation of the CloudFlare Client Interface API to flush content cache and restore originating IPs.',
 	'category' => 'service',
 	'author' => 'Xavier Perseguers',
 	'author_company' => 'Causal Sàrl',
