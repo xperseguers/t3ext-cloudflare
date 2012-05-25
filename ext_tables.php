caf3ad1030f9aaacc62b62364631c966ae12671f
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-25 18:15:55 +0000

    [TASK] Add custom CSS for our menu in toolbar
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cloudflare/trunk@62851 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -50,6 +50,9 @@ $icons = array(
 );
 t3lib_SpriteManager::addSingleIcons($icons, $_EXTKEY);
 
+// Register our custom CSS
+$GLOBALS['TBE_STYLES']['skins'][$_EXTKEY]['stylesheetDirectories']['visual'] = 'EXT:' . $_EXTKEY . '/Resources/Public/Css/visual/';
+
 t3lib_extMgm::registerExtDirectComponent(
 	'TYPO3.Ajax.ExtDirect.CloudflareToolbarMenu',
 	t3lib_extMgm::extPath($_EXTKEY) . 'Classes/ExtDirect/ToolbarMenu.php:Tx_Cloudflare_ExtDirect_ToolbarMenu',
