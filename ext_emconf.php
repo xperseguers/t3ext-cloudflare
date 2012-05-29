a4cdf2c637a70bf029c91988783a6fcf46a9af17
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-29 20:15:10 +0000

    [TASK] Update md5 checksums
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cloudflare/trunk@62922 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "cloudflare".
 #
-# Auto generated 22-05-2012 16:49
+# Auto generated 29-05-2012 22:14
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"d546";s:21:"ext_conf_template.txt";s:4:"5b33";s:12:"ext_icon.gif";s:4:"f7d7";s:17:"ext_localconf.php";s:4:"3b25";s:14:"ext_tables.php";s:4:"cbda";s:14:"doc/manual.sxw";s:4:"e99f";s:37:"hooks/class.tx_cloudflare_tcemain.php";s:4:"9c6a";}',
+	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"aafb";s:16:"ext_autoload.php";s:4:"c81b";s:21:"ext_conf_template.txt";s:4:"9c25";s:12:"ext_icon.gif";s:4:"f7d7";s:17:"ext_localconf.php";s:4:"69d6";s:14:"ext_tables.php";s:4:"2562";s:28:"Classes/EM/Configuration.php";s:4:"66c2";s:43:"Classes/ExtDirect/CloudflareToolbarItem.php";s:4:"2825";s:33:"Classes/ExtDirect/ToolbarMenu.php";s:4:"d457";s:25:"Classes/Hooks/TCEmain.php";s:4:"ec45";s:30:"Classes/Hooks/TYPO3backend.php";s:4:"817a";s:31:"Classes/Services/Cloudflare.php";s:4:"9ba9";s:39:"Resources/Public/Css/visual/toolbar.css";s:4:"ac64";s:40:"Resources/Public/Icons/cloudflare-16.png";s:4:"6365";s:36:"Resources/Public/Icons/direct-16.png";s:4:"923b";s:37:"Resources/Public/Icons/offline-16.png";s:4:"326b";s:36:"Resources/Public/Icons/online-16.png";s:4:"9216";s:45:"Resources/Public/JavaScript/cloudflaremenu.js";s:4:"dea1";s:14:"doc/manual.sxw";s:4:"1ab2";}',
 	'suggests' => array(
 	),
 );
