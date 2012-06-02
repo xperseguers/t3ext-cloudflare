21ed9fc32becf07cd3b1c27dca0754112e1b577f
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-02 14:27:38 +0000

    [TASK] Remove SSL redirect option as it can be done at the web server level or as page rules on CloudFlare
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cloudflare/trunk@63038 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -46,9 +46,6 @@ if ($isProxied) {
 		if ($cloudflareVisitor['scheme'] === 'https') {
 			$_SERVER['HTTPS'] = 'on';
 			$_SERVER['HTTP_X_FORWARDED_PROTO'] = 'https';
-		} elseif (TYPO3_MODE === 'FE' && isset($config['enforceSsl']) && $config['enforceSsl'] == 1) {
-			$url = 'https://' . t3lib_div::getIndpEnv('HTTP_HOST') . t3lib_div::getIndpEnv('REQUEST_URI');
-			t3lib_utility_Http::redirect($url);
 		}
 	}
 
