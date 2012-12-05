19fa20ac607a2bc98ba11d683dcd18ff5a5e37a8
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-12-05 13:25:31 +0000

    [FEATURE] #43612: Implement "zone_file_purge"
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cloudflare/trunk@68301 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Hooks/TCEmain.php
+++ Classes/Hooks/TCEmain.php
@@ -56,29 +56,38 @@ class Tx_Cloudflare_Hooks_TCEmain {
 	 * @return void
 	 */
 	public function clear_cacheCmd(array $params, t3lib_TCEmain $pObj) {
-		if ($params['cacheCmd'] !== 'all') {
-			return;
+		if ($params['cacheCmd'] === 'all') {
+			$this->clearCloudFlareCache($pObj->BE_USER);
+		} elseif (!empty($this->config['enablePurgeSingleFile'])) {
+			$pageUid = intval($params['cacheCmd']);
+			if ($pageUid) {
+				$url = $this->getFrontendUrl($pageUid);
+				if ($url) {
+					$this->purgeCloudFlareSingleFile(
+						isset($GLOBALS['BE_USER']) ? $GLOBALS['BE_USER'] : NULL,
+						$url
+					);
+				}
+			}
 		}
-
-		$this->clearCloudflareCache($pObj->BE_USER);
 	}
 
 	/**
-	 * Answers to AJAX call invoked when clearing only Cloud Flare cache.
+	 * Answers to AJAX call invoked when clearing only CloudFlare cache.
 	 *
 	 * @return void
 	 */
 	public function clearCache() {
-		$this->clearCloudflareCache(isset($GLOBALS['BE_USER']) ? $GLOBALS['BE_USER'] : NULL);
+		$this->clearCloudFlareCache(isset($GLOBALS['BE_USER']) ? $GLOBALS['BE_USER'] : NULL);
 	}
 
 	/**
-	 * Clear the Cloud Flare cache.
+	 * Clears the CloudFlare cache.
 	 *
 	 * @param t3lib_beUserAuth $beUser
 	 * @return void
 	 */
-	protected function clearCloudflareCache(t3lib_beUserAuth $beUser = NULL) {
+	protected function clearCloudFlareCache(t3lib_beUserAuth $beUser = NULL) {
 		$domains = $this->config['domains'] ? t3lib_div::trimExplode(',', $this->config['domains'], TRUE) : array();
 
 		/** @var $cloudflare Tx_Cloudflare_Services_Cloudflare */
@@ -104,6 +113,151 @@ class Tx_Cloudflare_Hooks_TCEmain {
 				if ($beUser !== NULL) {
 					$beUser->writelog(4, 1, 1, 0, $e->getMessage(), array());
 				}
+			}
+		}
+	}
+
+	/**
+	 * Returns a Frontend URL corresponding to a given page UID.
+	 * Implementation was inspired by EXT:vara_feurlfrombe
+	 *
+	 * @param integer $uid
+	 * @return string
+	 * @todo Add support for multiple Frontend URLs
+	 */
+	protected function getFrontendUrl($uid) {
+		if (isset($GLOBALS['BE_USER']) && $GLOBALS['BE_USER']->workspace != 0) {
+			// Preview in workspaces is not supported!
+			return NULL;
+		}
+
+		$GLOBALS['TSFE'] = t3lib_div::makeInstance('tslib_fe', $GLOBALS['TYPO3_CONF_VARS'], $uid, '');
+
+		$GLOBALS['TT'] = t3lib_div::makeInstance('t3lib_timeTrack');
+		$GLOBALS['TT']->start();
+		$GLOBALS['TSFE']->config['config']['language'] = 'default';
+
+		// Fire all the required function to get the TYPO3 Frontend all set up
+		$GLOBALS['TSFE']->id = $uid;
+		$GLOBALS['TSFE']->connectToDB();
+
+		// Prevent database debug messages from messing up the output
+		$GLOBALS['TYPO3_DB']->debugOutput = FALSE;
+
+		$GLOBALS['TSFE']->initLLVars();
+		$GLOBALS['TSFE']->initFEuser();
+
+		// Look up the page
+		$GLOBALS['TSFE']->sys_page = t3lib_div::makeInstance('t3lib_pageSelect');
+		$GLOBALS['TSFE']->sys_page->init($GLOBALS['TSFE']->showHiddenPage);
+
+		// If the page is not found (if the page is a sysfolder, etc), then return no URL,
+		// preventing any further processing which would result in an error page.
+		$page = $GLOBALS['TSFE']->sys_page->getPage($uid);
+
+		if (count($page) == 0) {
+			return NULL;
+		}
+
+		// If the page is a shortcut, look up the page to which the shortcut references,
+		// and do the same check as above.
+		if ($page['doktype'] == 4 && count($GLOBALS['TSFE']->getPageShortcut($page['shortcut'],$page['shortcut_mode'], $page['uid'])) == 0) {
+			return NULL;
+		}
+
+		// Spacer pages and sysfolders result in a page not found page too...
+		if ($page['doktype'] == 199 || $page['doktype'] == 254) {
+			return NULL;
+		}
+
+		$GLOBALS['TSFE']->getPageAndRootline();
+		$GLOBALS['TSFE']->initTemplate();
+		$GLOBALS['TSFE']->forceTemplateParsing = 1;
+
+		// Find the root template
+		$GLOBALS['TSFE']->tmpl->start($GLOBALS['TSFE']->rootLine);
+
+		// Fill the pSetup from the same variables from the same location as where
+		// tslib_fe->getConfigArray will get them, so they can be checked before
+		// this function is called
+		//$GLOBALS['TSFE']->sPre = $GLOBALS['TSFE']->tmpl->setup['types.'][$GLOBALS['TSFE']->type];	 // toplevel - objArrayName
+		//$GLOBALS['TSFE']->pSetup = $GLOBALS['TSFE']->tmpl->setup[$GLOBALS['TSFE']->sPre . '.'];
+
+		// If there is no root template found, there is no point in continuing which would
+		// result in a 'template not found' page and then call exit PHP.
+		// And the same applies if pSetup is empty, which would result in a
+		// "The page is not configured" message.
+		if (!$GLOBALS['TSFE']->tmpl->loaded || ($GLOBALS['TSFE']->tmpl->loaded && !$GLOBALS['TSFE']->pSetup)) {
+			//return NULL;
+		}
+
+		$GLOBALS['TSFE']->checkAlternativeIdMethods();
+		$GLOBALS['TSFE']->determineId();
+		$GLOBALS['TSFE']->getConfigArray();
+
+		// Get linkVars, absRefPrefix, etc
+		TSpagegen::pagegenInit();
+
+		/** @var $contentObj tslib_cObj */
+		$contentObj = t3lib_div::makeInstance('tslib_cObj');
+		$contentObj->start(array(), '');
+
+		// Create the URL
+		$link = $contentObj->typolink('', array(
+			'parameter'        => $uid,
+			'forceAbsoluteUrl' => 1,
+		));
+		$url = $contentObj->lastTypoLinkUrl;
+
+		return $url;
+	}
+
+	/**
+	 * Purges a single file in CloudFlare cache.
+	 * @param t3lib_beUserAuth $beUser
+	 * @param string $url
+	 * @return void
+	 */
+	protected function purgeCloudFlareSingleFile(t3lib_beUserAuth $beUser = NULL, $url) {
+		$domains = $this->config['domains'] ? t3lib_div::trimExplode(',', $this->config['domains'], TRUE) : array();
+
+		$isValidUrl = FALSE;
+		$domain = NULL;
+
+		if (preg_match('#^https?://([^/]+)#', $url, $matches)) {
+			$domainParts = explode('.', $matches[1]);
+			if (count($domainParts) > 1) {
+				$size = count($domainParts);
+				$domain = $domainParts[$size - 2] . '.' . $domainParts[$size - 1];
+				$isValidUrl = in_array($domain, $domains);
+			}
+		}
+
+		if (!$isValidUrl) {
+			return;
+		}
+
+		/** @var $cloudflare Tx_Cloudflare_Services_Cloudflare */
+		$cloudflare = t3lib_div::makeInstance('Tx_Cloudflare_Services_Cloudflare', $this->config);
+
+		$parameters = array(
+			'a'   => 'zone_file_purge',
+			'z'   => $domain,
+			'url' => $url,
+		);
+		try {
+			$ret = $cloudflare->send($parameters);
+
+			if ($beUser !== NULL) {
+				if ($ret['result'] === 'error') {
+					$beUser->writelog(4, 1, 1, 0, 'User %s failed to clear the cache on CloudFlare (domain: "%s") for "%s": %s', array($beUser->user['username'], $domain, $url, $ret['msg']));
+				} else {
+					$beUser->writelog(4, 1, 0, 0, 'User %s cleared the cache on CloudFlare (domain: "%s") for "%s"', array($beUser->user['username'], $domain, $url));
+				}
+			}
+		} catch (RuntimeException $e) {
+			if ($beUser !== NULL) {
+				$beUser->writelog(4, 1, 1, 0, $e->getMessage(), array());
 			}
 		}
 	}
