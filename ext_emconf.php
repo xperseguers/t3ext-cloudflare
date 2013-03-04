3c5efd1f23d26fe069c6b38b3be8ddaec3ec6d9a
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-04 10:43:40 +0000

    [TASK] Extension has been tested with TYPO3 6.1 (master)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cloudflare/branches/ExtJS_Menu@72177 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.2.2-dev',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5.0-4.7.99',
+			'typo3' => '4.5.0-6.1.99',
 		),
 		'conflicts' => array(
 		),
