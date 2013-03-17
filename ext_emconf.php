e607150c4eb3849f4cc23bbbd41839449c113427
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-17 18:13:59 +0000

    [TASK] Raise version to 1.2.3
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cloudflare/trunk@73187 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,10 +29,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '1.2.2-dev',
+	'version' => '1.2.3',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5.0-4.7.99',
+			'typo3' => '4.5.0-6.1.99',
 		),
 		'conflicts' => array(
 		),
