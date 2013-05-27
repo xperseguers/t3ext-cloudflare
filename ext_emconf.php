d4ce5889ee093d386294844c4347fe4e7d1f1d9d
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-05-27 06:06:55 +0000

    [TASK] Use CloudFlare's favorite icon
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cloudflare/trunk@75535 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,6 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.2.5',
 	'constraints' => array(
 		'depends' => array(
+			'php' => '5.3.3-0.0.0',
 			'typo3' => '4.5.0-6.1.99',
 		),
 		'conflicts' => array(
