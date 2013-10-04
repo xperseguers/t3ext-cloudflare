5811d29d2bb5b4bd1656ea23f2fbe727d0051e91
Xavier Perseguers <xavier@typo3.org>
2013-10-04 09:17:29 +0200

    [TASK] Change compatibility up to 6.2.99
    Change-Id: I5829eab1d36d5efc3fc5888eccb6cb8bda2d5674
    Reviewed-on: https://review.typo3.org/24351
    Reviewed-by: Xavier Perseguers
    Tested-by: Xavier Perseguers

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.3-0.0.0',
-			'typo3' => '4.5.0-6.1.99',
+			'typo3' => '4.5.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
