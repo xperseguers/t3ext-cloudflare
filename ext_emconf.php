576402aeec913ae353bd1219e3cedd930c35df0f
Xavier Perseguers <xavier@typo3.org>
2015-10-27 07:15:09 +0100

    [TASK] ext_emconf.php should contain an autoload section
    Change-Id: Ie3078a41839aceb126645e0a6b2a9afd792078ec
    Reviewed-on: https://review.typo3.org/44311
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- ext_emconf.php
+++ ext_emconf.php
@@ -40,4 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
     ),
     '_md5_values_when_last_written' => '',
     'suggests' => array(),
+    'autoload' => array(
+        'psr-4' => array('Causal\\Cloudflare\\' => 'Classes')
+    ),
 );
