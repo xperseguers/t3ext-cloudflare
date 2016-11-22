90d4e22ad6e194e64f540238e09c1f7cd631b810
Xavier Perseguers <xavier@causal.ch>
2016-11-22 09:07:42 +0100

    [TASK] Use boot() method in initialization scripts
    Change-Id: Ibf5b4377d7180be333c7b87671fe326ece825b9c
    Reviewed-on: https://review.typo3.org/50733
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,89 +1,94 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-$config = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
-if (!is_array($config)) {
-    $config = [];
-}
-
-// Register additional clear_cache method
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] = 'EXT:' . $_EXTKEY . '/Classes/Hooks/TCEmain.php:Causal\\Cloudflare\\Hooks\\TCEmain->clear_cacheCmd';
-
-$remoteIp = \TYPO3\CMS\Core\Utility\GeneralUtility::getIndpEnv('REMOTE_ADDR');
-
-// @see https://www.cloudflare.com/ips
-$whiteListIPv4s = [
-    '103.21.244.0/22',
-    '103.22.200.0/22',
-    '103.31.4.0/22',
-    '104.16.0.0/12',
-    '108.162.192.0/18',
-    '131.0.72.0/22',
-    '141.101.64.0/18',
-    '162.158.0.0/15',
-    '172.64.0.0/13',
-    '173.245.48.0/20',
-    '188.114.96.0/20',
-    '190.93.240.0/20',
-    '197.234.240.0/22',
-    '198.41.128.0/17',
-    '199.27.128.0/21',
-];
-$whiteListIPv6s = [
-    '2400:cb00::/32',
-    '2405:8100::/32',
-    '2405:b500::/32',
-    '2606:4700::/32',
-    '2803:f800::/32',
-    '2c0f:f248::/32',
-    '2a06:98c0::/29',
-];
-
-$isProxied = false;
-if (isset($config['enableOriginatingIPs']) && $config['enableOriginatingIPs'] == 1) {
-    if (\TYPO3\CMS\Core\Utility\GeneralUtility::validIPv6($remoteIp)) {
-        $isProxied |= \TYPO3\CMS\Core\Utility\GeneralUtility::cmpIPv6($remoteIp, implode(',', $whiteListIPv6s));
-    } else {
-        $isProxied |= \TYPO3\CMS\Core\Utility\GeneralUtility::cmpIPv4($remoteIp, implode(',', $whiteListIPv4s));
-    }
-} elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    // We take for granted that reverse-proxy is properly configured
-    $isProxied = true;
-}
-
-if ($isProxied) {
-    // Flexible-SSL support
-    if (isset($_SERVER['HTTP_CF_VISITOR'])) {
-        $cloudflareVisitor = json_decode($_SERVER['HTTP_CF_VISITOR'], true);
-        if ($cloudflareVisitor['scheme'] === 'https') {
-            $_SERVER['HTTPS'] = 'on';
-            $_SERVER['HTTP_X_FORWARDED_PROTO'] = 'https';
-            $_SERVER['SERVER_PORT'] = '443';
-        }
+$boot = function ($_EXTKEY) {
+    $config = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
+    if (!is_array($config)) {
+        $config = [];
     }
 
-    // Cache SSL content
-    if (isset($config['cacheSslContent']) && $config['cacheSslContent'] == 1) {
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['nc_staticfilecache/class.tx_ncstaticfilecache.php']['createFile_initializeVariables'][] = 'EXT:' . $_EXTKEY . '/Classes/Hooks/tx_ncstaticfilecache.php:Tx_Cloudflare_Hooks_NcStaticfilecache->createFile_initializeVariables';
-    }
+    // Register additional clear_cache method
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] = 'EXT:' . $_EXTKEY . '/Classes/Hooks/TCEmain.php:Causal\\Cloudflare\\Hooks\\TCEmain->clear_cacheCmd';
 
+    $remoteIp = \TYPO3\CMS\Core\Utility\GeneralUtility::getIndpEnv('REMOTE_ADDR');
+
+    // @see https://www.cloudflare.com/ips
+    $whiteListIPv4s = [
+        '103.21.244.0/22',
+        '103.22.200.0/22',
+        '103.31.4.0/22',
+        '104.16.0.0/12',
+        '108.162.192.0/18',
+        '131.0.72.0/22',
+        '141.101.64.0/18',
+        '162.158.0.0/15',
+        '172.64.0.0/13',
+        '173.245.48.0/20',
+        '188.114.96.0/20',
+        '190.93.240.0/20',
+        '197.234.240.0/22',
+        '198.41.128.0/17',
+        '199.27.128.0/21',
+    ];
+    $whiteListIPv6s = [
+        '2400:cb00::/32',
+        '2405:8100::/32',
+        '2405:b500::/32',
+        '2606:4700::/32',
+        '2803:f800::/32',
+        '2c0f:f248::/32',
+        '2a06:98c0::/29',
+    ];
+
+    $isProxied = false;
     if (isset($config['enableOriginatingIPs']) && $config['enableOriginatingIPs'] == 1) {
-        if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
-            $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
+        if (\TYPO3\CMS\Core\Utility\GeneralUtility::validIPv6($remoteIp)) {
+            $isProxied |= \TYPO3\CMS\Core\Utility\GeneralUtility::cmpIPv6($remoteIp, implode(',', $whiteListIPv6s));
+        } else {
+            $isProxied |= \TYPO3\CMS\Core\Utility\GeneralUtility::cmpIPv4($remoteIp, implode(',', $whiteListIPv4s));
+        }
+    } elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        // We take for granted that reverse-proxy is properly configured
+        $isProxied = true;
+    }
+
+    if ($isProxied) {
+        // Flexible-SSL support
+        if (isset($_SERVER['HTTP_CF_VISITOR'])) {
+            $cloudflareVisitor = json_decode($_SERVER['HTTP_CF_VISITOR'], true);
+            if ($cloudflareVisitor['scheme'] === 'https') {
+                $_SERVER['HTTPS'] = 'on';
+                $_SERVER['HTTP_X_FORWARDED_PROTO'] = 'https';
+                $_SERVER['SERVER_PORT'] = '443';
+            }
+        }
+
+        // Cache SSL content
+        if (isset($config['cacheSslContent']) && $config['cacheSslContent'] == 1) {
+            $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['nc_staticfilecache/class.tx_ncstaticfilecache.php']['createFile_initializeVariables'][] = 'EXT:' . $_EXTKEY . '/Classes/Hooks/tx_ncstaticfilecache.php:Tx_Cloudflare_Hooks_NcStaticfilecache->createFile_initializeVariables';
+        }
+
+        if (isset($config['enableOriginatingIPs']) && $config['enableOriginatingIPs'] == 1) {
+            if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+                $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
+            }
         }
     }
-}
 
-if (TYPO3_MODE === 'BE' && !empty($config['apiKey'])) {
-    if (version_compare(TYPO3_version, '6.99.99', '<=')) {
-        $cloudflareToolbarItemClassPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY, 'Classes/Hooks/TYPO3backend_Cloudflare.php');
-        $GLOBALS['TYPO3_CONF_VARS']['typo3/backend.php']['additionalBackendItems'][] = $cloudflareToolbarItemClassPath;
-    } else {
-        $GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][] = 'Causal\\Cloudflare\\Backend\\ToolbarItems\\CloudflareToolbarItem';
-    }
+    if (TYPO3_MODE === 'BE' && !empty($config['apiKey'])) {
+        if (version_compare(TYPO3_version, '6.99.99', '<=')) {
+            $cloudflareToolbarItemClassPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY, 'Classes/Hooks/TYPO3backend_Cloudflare.php');
+            $GLOBALS['TYPO3_CONF_VARS']['typo3/backend.php']['additionalBackendItems'][] = $cloudflareToolbarItemClassPath;
+        } else {
+            $GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][] = 'Causal\\Cloudflare\\Backend\\ToolbarItems\\CloudflareToolbarItem';
+        }
 
-    if ($config['domains'] !== '') {
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions']['clearCloudflareCache'] = 'EXT:' . $_EXTKEY . '/Classes/Hooks/TYPO3backend.php:Causal\\Cloudflare\\Hooks\\TYPO3backend';
-        $GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['cloudflare::clearCache'] = 'EXT:' . $_EXTKEY . '/Classes/Hooks/TCEmain.php:Causal\\Cloudflare\\Hooks\\TCEmain->clearCache';
+        if ($config['domains'] !== '') {
+            $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions']['clearCloudflareCache'] = 'EXT:' . $_EXTKEY . '/Classes/Hooks/TYPO3backend.php:Causal\\Cloudflare\\Hooks\\TYPO3backend';
+            $GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['cloudflare::clearCache'] = 'EXT:' . $_EXTKEY . '/Classes/Hooks/TCEmain.php:Causal\\Cloudflare\\Hooks\\TCEmain->clearCache';
+        }
     }
-}
+};
+
+$boot($_EXTKEY);
+unset($boot);
--- ext_tables.php
+++ ext_tables.php
@@ -1,92 +1,114 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-// Register additional sprite icons
-$icons = [
-    'cloudflare' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/cloudflare-16.png',
-    'direct' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/direct-16.png',
-    'offline' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/offline-16.png',
-    'online' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/online-16.png',
-    'module' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/module-cloudflare.png',
-];
-if (version_compare(TYPO3_version, '7.6', '>=')) {
-    /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Imaging\\IconRegistry');
-    foreach ($icons as $key => $icon) {
-        $iconRegistry->registerIcon('extensions-' . $_EXTKEY . '-' . $key,
-            'TYPO3\\CMS\\Core\\Imaging\\IconProvider\\BitmapIconProvider',
+$boot = function ($_EXTKEY) {
+    // Register additional sprite icons
+    $icons = [
+        'cloudflare' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/cloudflare-16.png',
+        'direct' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/direct-16.png',
+        'offline' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/offline-16.png',
+        'online' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/online-16.png',
+        'module' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/module-cloudflare.png',
+    ];
+    if (version_compare(TYPO3_version, '7.6', '>=')) {
+        /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
+        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Imaging\\IconRegistry');
+        foreach ($icons as $key => $icon) {
+            $iconRegistry->registerIcon('extensions-' . $_EXTKEY . '-' . $key,
+                'TYPO3\\CMS\\Core\\Imaging\\IconProvider\\BitmapIconProvider',
+                [
+                    'source' => $icon
+                ]
+            );
+        }
+        unset($iconRegistry);
+    } else {
+        $extensionRelativePath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY);
+        $icons = array_map(
+            function ($e) use ($_EXTKEY, $extensionRelativePath) {
+                return str_replace('EXT:' . $_EXTKEY . '/', $extensionRelativePath, $e);
+            },
+            $icons
+        );
+        \TYPO3\CMS\Backend\Sprite\SpriteManager::addSingleIcons($icons, $_EXTKEY);
+    }
+
+    // Register our custom CSS
+    $GLOBALS['TBE_STYLES']['skins'][$_EXTKEY]['stylesheetDirectories']['visual'] = 'EXT:' . $_EXTKEY . '/Resources/Public/Css/visual/';
+
+    if (TYPO3_MODE === 'BE') {
+        if (version_compare(TYPO3_version, '6.99.99', '<=')) {
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerExtDirectComponent(
+                'TYPO3.Ajax.ExtDirect.CloudflareToolbarMenu',
+                \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/ExtDirect/ToolbarMenu.php:Causal\\Cloudflare\\ExtDirect\\ToolbarMenu',
+                null,
+                'admin'
+            );
+
+            // Register AJAX calls
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler(
+                'TxCloudflare::purge',
+                'Causal\\Cloudflare\\ExtDirect\\ToolbarMenu->purge'
+            );
+        } else {
+            // Register AJAX calls
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler(
+                'TxCloudflare::renderMenu',
+                'Causal\\Cloudflare\\Backend\\ToolbarItems\\CloudflareToolbarItem->renderAjax'
+            );
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler(
+                'TxCloudflare::toggleDevelopmentMode',
+                'Causal\\Cloudflare\\Backend\\ToolbarItems\\CloudflareToolbarItem->toggleDevelopmentMode'
+            );
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler(
+                'TxCloudflare::purge',
+                'Causal\\Cloudflare\\Backend\\ToolbarItems\\CloudflareToolbarItem->purge'
+            );
+        }
+
+        // Create a module section "Cloudflare" before 'Admin Tools'
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
+            'txcloudflare', // main module key
+            '',             // submodule key
+            '',             // position
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Modules/Cloudflare/'
+        );
+        $temp_TBE_MODULES = [];
+        foreach ($GLOBALS['TBE_MODULES'] as $key => $val) {
+            if ($key === 'tools') {
+                $temp_TBE_MODULES['txcloudflare'] = '';
+                $temp_TBE_MODULES[$key] = $val;
+            } else {
+                $temp_TBE_MODULES[$key] = $val;
+            }
+        }
+        $GLOBALS['TBE_MODULES'] = $temp_TBE_MODULES;
+        $GLOBALS['TBE_MODULES']['_configuration']['txcloudflare'] = [
+            'labels' => 'LLL:EXT:cloudflare/Resources/Private/Language/locallang_mod_cloudflare.xlf',
+            'iconIdentifier' => 'extensions-cloudflare-module',
+        ];
+
+        if (version_compare(TYPO3_version, '6.99.99', '<=')) {
+            $moduleIcon = 'ext_icon.png';
+        } else {
+            $moduleIcon = 'Resources/Public/Icons/module-analytics.png';
+        }
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+            'Causal.' . $_EXTKEY,
+            'txcloudflare',
+            'analytics',
+            '',
             [
-                'source' => $icon
+                'Dashboard' => 'analytics, ajaxAnalytics',
+            ],
+            [
+                'access' => 'user,group',
+                'icon' => 'EXT:' . $_EXTKEY . '/' . $moduleIcon,
+                'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_mod_analytics.xlf',
             ]
         );
     }
-    unset($iconRegistry);
-} else {
-    $extensionRelativePath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY);
-    $icons = array_map(
-        function ($e) use ($_EXTKEY, $extensionRelativePath) {
-            return str_replace('EXT:' . $_EXTKEY . '/', $extensionRelativePath, $e);
-        },
-        $icons
-    );
-    \TYPO3\CMS\Backend\Sprite\SpriteManager::addSingleIcons($icons, $_EXTKEY);
-}
+};
 
-// Register our custom CSS
-$GLOBALS['TBE_STYLES']['skins'][$_EXTKEY]['stylesheetDirectories']['visual'] = 'EXT:' . $_EXTKEY . '/Resources/Public/Css/visual/';
-
-if (TYPO3_MODE === 'BE') {
-    if (version_compare(TYPO3_version, '6.99.99', '<=')) {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerExtDirectComponent(
-            'TYPO3.Ajax.ExtDirect.CloudflareToolbarMenu',
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/ExtDirect/ToolbarMenu.php:Causal\\Cloudflare\\ExtDirect\\ToolbarMenu',
-            null,
-            'admin'
-        );
-
-        // Register AJAX calls
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler('TxCloudflare::purge', 'Causal\\Cloudflare\\ExtDirect\\ToolbarMenu->purge');
-    } else {
-        // Register AJAX calls
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler('TxCloudflare::renderMenu', 'Causal\\Cloudflare\\Backend\\ToolbarItems\\CloudflareToolbarItem->renderAjax');
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler('TxCloudflare::toggleDevelopmentMode', 'Causal\\Cloudflare\\Backend\\ToolbarItems\\CloudflareToolbarItem->toggleDevelopmentMode');
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler('TxCloudflare::purge', 'Causal\\Cloudflare\\Backend\\ToolbarItems\\CloudflareToolbarItem->purge');
-    }
-
-    // Create a module section "Cloudflare" before 'Admin Tools'
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule('txcloudflare', '', '', \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Modules/Cloudflare/');
-    $temp_TBE_MODULES = [];
-    foreach ($GLOBALS['TBE_MODULES'] as $key => $val) {
-        if ($key === 'tools') {
-            $temp_TBE_MODULES['txcloudflare'] = '';
-            $temp_TBE_MODULES[$key] = $val;
-        } else {
-            $temp_TBE_MODULES[$key] = $val;
-        }
-    }
-    $GLOBALS['TBE_MODULES'] = $temp_TBE_MODULES;
-    $GLOBALS['TBE_MODULES']['_configuration']['txcloudflare'] = [
-        'labels' => 'LLL:EXT:cloudflare/Resources/Private/Language/locallang_mod_cloudflare.xlf',
-        'iconIdentifier' => 'extensions-cloudflare-module',
-    ];
-
-    if (version_compare(TYPO3_version, '6.99.99', '<=')) {
-        $moduleIcon = 'ext_icon.png';
-    } else {
-        $moduleIcon = 'Resources/Public/Icons/module-analytics.png';
-    }
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'Causal.' . $_EXTKEY,
-        'txcloudflare',
-        'analytics',
-        '',
-        [
-            'Dashboard' => 'analytics, ajaxAnalytics',
-        ],
-        [
-            'access' => 'user,group',
-            'icon' => 'EXT:' . $_EXTKEY . '/' . $moduleIcon,
-            'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_mod_analytics.xlf',
-        ]
-    );
-}
+$boot($_EXTKEY);
+unset($boot);
