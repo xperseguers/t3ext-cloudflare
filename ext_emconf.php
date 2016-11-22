647d838a6d3c4a9dbb636206d231480c47b8e379
Xavier Perseguers <xavier@causal.ch>
2016-11-22 08:45:16 +0100

    [!!!][TASK] Raise version to 2.1.0-dev
    Change-Id: Ifec07f91e5cf50a2325cd2fb9173fec2f939faeb
    Reviewed-on: https://review.typo3.org/50730
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Cloudflare Client',
     'description' => 'The Cloudflare TYPO3 extension ensures your TYPO3 website is running optimally on the Cloudflare platform.',
     'category' => 'services',
@@ -29,18 +29,18 @@ $EM_CONF[$_EXTKEY] = array(
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '2.0.5-dev',
-    'constraints' => array(
-        'depends' => array(
-            'php' => '5.3.3-7.0.99',
+    'version' => '2.1.0-dev',
+    'constraints' => [
+        'depends' => [
+            'php' => '5.5.0-7.0.99',
             'typo3' => '6.2.0-8.99.99',
-        ),
-        'conflicts' => array(),
-        'suggests' => array(),
-    ),
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
     '_md5_values_when_last_written' => '',
-    'suggests' => array(),
-    'autoload' => array(
-        'psr-4' => array('Causal\\Cloudflare\\' => 'Classes')
-    ),
-);
+    'suggests' => [],
+    'autoload' => [
+        'psr-4' => ['Causal\\Cloudflare\\' => 'Classes']
+    ],
+];
