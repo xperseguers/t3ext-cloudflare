51fa09bcb5d0f7c681cb908eac54208e2ed6a772
Andriy Oprysko <andriy@pixelant.se>
2018-02-21 14:36:06 +0200

    [TASK] added SingletonInterface for DataUrlModifierBackwardCompatibility

--- Classes/Solr/DataUrlModifierBackwardCompatibility.php
+++ Classes/Solr/DataUrlModifierBackwardCompatibility.php
@@ -1,11 +1,12 @@
 <?php
 namespace Causal\Cloudflare\Solr;
+use TYPO3\CMS\Core\SingletonInterface;
 
 /**
  * Class DataUrlModifierBackwardCompatibility
  *
  * @package Causal\Cloudflare\Solr
  */
-class DataUrlModifierBackwardCompatibility extends AbstractDataUrlModifier implements \Tx_Solr_IndexQueuePageIndexerDataUrlModifier
+class DataUrlModifierBackwardCompatibility extends AbstractDataUrlModifier implements \Tx_Solr_IndexQueuePageIndexerDataUrlModifier, SingletonInterface
 {
 }
