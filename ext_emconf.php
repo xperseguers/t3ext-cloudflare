bd82cdfdae2eb19cc9f1d44c067e181434b3dd1d
Xavier Perseguers <xavier@causal.ch>
2024-06-11 09:19:19 +0200

    [TASK] Mark compatibility with PHP 8.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.5.1',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.2.99',
+            'php' => '7.4.0-8.3.99',
             'typo3' => '10.4.0-11.5.99',
         ],
         'conflicts' => [],
